c30018e91610b0896ea6c822e2fd44f61c3ac9fb
NahuelBarreto04 <nahuelbarreto54@gmail.com>
2023-02-09 01:13:45 -0300

    restyling message component

--- client/src/pages/message/MessageHistory.tsx
+++ client/src/pages/message/MessageHistory.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import Message from './Message'
+import React from "react";
+import Message from "../../components/Message";
 
 export default function MessageHistory() {
-    return (
-        <div className='1048:px-36'>
-            <Message />
+  const objMenssage = {
+    by: "admin",
+  };
+  return (
+    <div className="flex h-[80vh] w-[90%] justify-center overflow-y-hidden pl-4">
+      <div className=" h-[100%] w-[90%] overflow-y-scroll rounded-2xl border-none bg-white py-4 px-6">
+        <h2 className="mb-4 text-2xl font-semibold">
+          Mensajes y Comunicaciones
+        </h2>
+        <div className="flex flex-col gap-y-5 ">
+          <Message by={objMenssage.by} clase="Reparacion" />
+          <Message by={objMenssage.by} clase="Comunicacion" />
+          <Message by={objMenssage.by} clase="Votacion-Activa" />
+          <Message by={objMenssage.by} clase="Votacion-Activa" />
+          <Message by={objMenssage.by} clase="Votacion-Activa" />
         </div>
-    )
+      </div>
+    </div>
+  );
 }

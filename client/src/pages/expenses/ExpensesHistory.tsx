00fe1d2c299e42720abc46aaa15079566f7952b0
Huichoman <rockbotica@gmail.com>
2023-02-15 23:38:00 -0600

    Modify ExpensesHistory.jsx

--- client/src/pages/expenses/ExpensesHistory.tsx
+++ client/src/pages/expenses/ExpensesHistory.tsx
@@ -1,8 +1,8 @@
 
 export default function ExpensesHistory() 
  {
-    const leftArrow = <svg width="50" height="50" className="fill-rose-500" viewBox="0 0 24 24"><path  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2Zm3.5 10.75h-5.19l1.72 1.72c.29.29.29.77 0 1.06-.15.15-.34.22-.53.22s-.38-.07-.53-.22l-3-3a.754.754 0 0 1 0-1.06l3-3c.29-.29.77-.29 1.06 0 .29.29.29.77 0 1.06l-1.72 1.72h5.19c.41 0 .75.34.75.75s-.34.75-.75.75Z"/></svg>;
-    const  rightArrow = <svg width="50" height="50" className="fill-emerald-500" viewBox="0 0 24 24"><path  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2Zm4.03 10.53-3 3c-.15.15-.34.22-.53.22s-.38-.07-.53-.22a.754.754 0 0 1 0-1.06l1.72-1.72H8.5c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h5.19l-1.72-1.72a.754.754 0 0 1 0-1.06c.29-.29.77-.29 1.06 0l3 3c.29.29.29.77 0 1.06Z"/></svg>;
+    const leftArrow = <svg width="20" height="20" className="fill-rose-500" viewBox="0 0 24 24"><path  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2Zm3.5 10.75h-5.19l1.72 1.72c.29.29.29.77 0 1.06-.15.15-.34.22-.53.22s-.38-.07-.53-.22l-3-3a.754.754 0 0 1 0-1.06l3-3c.29-.29.77-.29 1.06 0 .29.29.29.77 0 1.06l-1.72 1.72h5.19c.41 0 .75.34.75.75s-.34.75-.75.75Z"/></svg>;
+    const  rightArrow = <svg width="20" height="20" className="fill-emerald-500" viewBox="0 0 24 24"><path  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2Zm4.03 10.53-3 3c-.15.15-.34.22-.53.22s-.38-.07-.53-.22a.754.754 0 0 1 0-1.06l1.72-1.72H8.5c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h5.19l-1.72-1.72a.754.754 0 0 1 0-1.06c.29-.29.77-.29 1.06 0l3 3c.29.29.29.77 0 1.06Z"/></svg>;
     return (
         <div className='w-2/4'>
             {/* <Expenses /> */}
@@ -20,31 +20,31 @@ export default function ExpensesHistory()
                 <div className="flex flex-row justify-between " >
                     <div className="basis-3/4 flex flex-arrow items-center"> <p className='text-left  font-poppins'>Diciembre 2022</p></div>
                     <div className="basis-1/4 items-end  flex flex-row items-center justify-end"><p className='text-right text-sm text-zinc-800'>10/01/2023</p></div>    
-                    <div className="basis-1/4 items-end  flex flex-row items-center justify-end"><p className='text-right text-sm text-zinc-800'>Pendiente</p></div>             
+                    <div className="basis-1/4 items-end  flex flex-row items-center justify-end">{leftArrow}<p className='text-right text-sm text-zinc-800'>Pendiente</p></div>             
                 </div>
 
                 <div className="flex flex-row justify-between " >
                     <div className="basis-3/4 flex flex-arrow items-center"> <p className='text-left  font-poppins'>Noviembre 2022</p></div>
                     <div className="basis-1/4 items-end  flex flex-row items-center justify-end"><p className='text-right text-sm text-zinc-800'>09/12/2022</p></div>  
-                    <div className="basis-1/4 items-end  flex flex-row items-center justify-end"><p className='text-right text-sm text-zinc-800'>Pagada</p></div>               
+                    <div className="basis-1/4 items-end  flex flex-row items-center justify-end">{rightArrow}<p className='text-right text-sm text-zinc-800'>Pagada</p></div>               
                 </div>
 
                 <div className="flex flex-row justify-between " >
                     <div className="basis-3/4 flex flex-arrow items-center"> <p className='text-left  font-poppins'>Octubre 2022</p></div>
                     <div className="basis-1/4 items-end  flex flex-row items-center justify-end"><p className='text-right text-sm text-zinc-800'>10/11/2022</p></div>     
-                    <div className="basis-1/4 items-end  flex flex-row items-center justify-end"><p className='text-right text-sm text-zinc-800'>Pagada</p></div>           
+                    <div className="basis-1/4 items-end  flex flex-row items-center justify-end">{rightArrow}<p className='text-right text-sm text-zinc-800'>Pagada</p></div>           
                 </div>
 
                 <div className="flex flex-row justify-between " >
                     <div className="basis-3/4 flex flex-arrow items-center"> <p className='text-left  font-poppins'>Septiembre 2022</p></div>
                     <div className="basis-1/4 items-end  flex flex-row items-center justify-end"><p className='text-right text-sm text-zinc-800'>12/10/2022</p></div>    
-                    <div className="basis-1/4 items-end  flex flex-row items-center justify-end"><p className='text-right text-sm text-zinc-800'>Pagada</p></div>            
+                    <div className="basis-1/4 items-end  flex flex-row items-center justify-end">{rightArrow}<p className='text-right text-sm text-zinc-800'>Pagada</p></div>            
                 </div>
 
                 <div className="flex flex-row justify-between " >
                     <div className="basis-3/4 flex flex-arrow items-center"> <p className='text-left  font-poppins'>Agosto 2022</p></div>
                     <div className="basis-1/4 items-end  flex flex-row items-center justify-end"><p className='text-right text-sm text-zinc-800'>10/09/2022</p></div>  
-                    <div className="basis-1/4 items-end  flex flex-row items-center justify-end"><p className='text-right text-sm text-zinc-800'>Pagada</p></div>              
+                    <div className="basis-1/4 items-end  flex flex-row items-center justify-end">{rightArrow}<p className='text-right text-sm text-zinc-800'>Pagada</p></div>              
                 </div>
 
 

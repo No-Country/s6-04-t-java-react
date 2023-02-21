9345bcdf170dd58abde28dea588267456b5ba741
Andres-Developer <andres.burbano.dev@gmail.com>
2023-02-21 13:23:40 -0500

    test api backend production

--- client/src/service/client.ts
+++ client/src/service/client.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL
+    // baseURL: import.meta.env.VITE_API_BASE_URL
+    baseURL: "https://consorcio-production.up.railway.app"
 });
 
 client.interceptors.response.use(response => response.data);

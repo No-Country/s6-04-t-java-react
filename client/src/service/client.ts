4e01c2beda353b8d113593dbc5470bc3c7f55c36
Andres-Developer <andres.burbano.dev@gmail.com>
2023-02-21 23:00:44 -0500

    mod api url to subdomain

--- client/src/service/client.ts
+++ client/src/service/client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const client = axios.create({
     // baseURL: import.meta.env.VITE_API_BASE_URL,
     // baseURL: "http://localhost:5000",
-    baseURL: "http://44.200.219.85:5000",
+    baseURL: "https://live-to.sytes.net/api",
     headers: {
       "Content-Type": "application/json",
       'Authorization': `Bearer ${localStorage.getItem("token")}`,

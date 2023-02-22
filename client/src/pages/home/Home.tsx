b9d369e849b48ef4043a7c76af62381805550b9f
Huichoman <rockbotica@gmail.com>
2023-02-22 13:56:10 -0600

    Expenses update

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,13 +1,14 @@
 import Amenities from "../amenities/Amenities";
 import Expenses from "../expenses/Expenses";
 import Message from "../../components/Message";
+import { BillingResume } from "../expenses/BillingResume";
 
 export default function Home() {
     return (
         // gap-y-4
         <div className="flex flex-col gap-y-4 1048:p-6 ">
             <div className="flex flex-col gap-y-4 1048:grid 1048:grid-cols-2 1048:gap-x-6 ">
-                <Expenses />
+                <BillingResume />
                 <Message />
             </div>
             <div className="md:mt-6">

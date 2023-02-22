946d4b95e306d7a5d6b947e383ae4dced20ee761
Andres-Developer <andres.burbano.dev@gmail.com>
2023-02-21 22:42:56 -0500

    mod base url to api host local

--- client/src/service/client.ts
+++ client/src/service/client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
 const client = axios.create({
-    // baseURL: import.meta.env.VITE_API_BASE_URL
-    baseURL: "https://consorcio-production.up.railway.app"
+    // baseURL: import.meta.env.VITE_API_BASE_URL,
+    // baseURL: "http://localhost:5000",
+    baseURL: "http://44.200.219.85:5000",
+    headers: {
+      "Content-Type": "application/json",
+      'Authorization': `Bearer ${localStorage.getItem("token")}`,
+    },
 });
 
 client.interceptors.response.use(response => response.data);

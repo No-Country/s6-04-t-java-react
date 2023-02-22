29aa3cf0c1c7181025f28b11cf0b3a5ac3fa58f0
Andres-Developer <andres.burbano.dev@gmail.com>
2023-02-22 14:51:33 -0500

    Using Prod API baseURL

--- client/src/service/client.ts
+++ client/src/service/client.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-    // baseURL: import.meta.env.VITE_API_BASE_URL,
-    // baseURL: "http://localhost:5000",
-    baseURL: "https://live-to.sytes.net/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
       "Content-Type": "application/json",
       'Authorization': `Bearer ${localStorage.getItem("token")}`,

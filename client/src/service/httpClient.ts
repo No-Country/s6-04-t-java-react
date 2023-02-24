9c2884869a8dd3a0d9f8bb3969ee9f9a86832bcf
Andres-Developer <andres.burbano.dev@gmail.com>
2023-02-24 15:00:16 -0500

    FIX: Cors error at GET request

--- client/src/service/httpClient.ts
+++ client/src/service/httpClient.ts
@@ -1,17 +1,26 @@
 import axios from "axios";
 
 const apiURL: string = import.meta.env.VITE_API_BASE_URL;
-const token = localStorage.getItem("token") || "";
-const Authorization = token && `Bearer ${token}`;
 
 const client = axios.create({
   baseURL: apiURL,
   headers: {
     "Content-Type": "application/json",
-    Authorization,
   },
 });
 const isClientError = axios.isAxiosError;
+
+client.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
 client.interceptors.response.use(response => response.data);
 
 export { client, isClientError };
